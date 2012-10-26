50f5b70742cff27b18c01fe5aec20a34a15f8b62
Douglas Sweetser <sweetser@alum.mit.edu>
2012-10-25 22:13:51 -0400

    Added potentially useful import packages: Before and After.

--- src/test/java/org/visualphysics/TemplateTest.java
+++ src/test/java/org/visualphysics/TemplateTest.java
@@ -1,7 +1,10 @@
 package org.visualphysics;
 
 import  org.visualphysics.Template;
+
+import  org.junit.Before;
 import  org.junit.Test;
+import  org.junit.After;
 import  static org.junit.Assert.*;
 
 public class TemplateTest {
