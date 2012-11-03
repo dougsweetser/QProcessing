5ffc6991d7612a501f779201f01de8429852306e
Douglas Sweetser <sweetser@alum.mit.edu>
2012-11-03 00:47:35 -0400

    Uses ints.

--- src/test/java/org/visualphysics/GoldenBirthdayYearTest.java
+++ src/test/java/org/visualphysics/GoldenBirthdayYearTest.java
@@ -12,16 +12,16 @@ public class GoldenBirthdayYearTest {
     @Test
     public void test_calculate_golden_birthday_year() {
 
-        String the_year = "2008";
-        String the_gby = "2016";
+        int the_year = 2008;
+        int the_gby = 2016;
         GoldenBirthdayYear gby = new GoldenBirthdayYear(the_year);
-        String gby_year = gby.calculate_golden_birthday_year();
+        int gby_year = gby.calculate_golden_birthday_year();
         assertEquals(the_gby, gby_year);
     }
 
     @Test
     public void test_print_golden_birthday_year() {
-        String the_year = "2008";
+        int the_year = 2008;
         String the_gby  = "2016";
         boolean pprint = false;
         GoldenBirthdayYear gby = new GoldenBirthdayYear(the_year);
@@ -31,7 +31,7 @@ public class GoldenBirthdayYearTest {
 
     @Test
     public void test_pprint_print_golden_birthday_year() {
-        String the_year = "2008";
+        int the_year = 2008;
         String the_pp_gby  = "Since your birth year is 2008, your golden birthday is 2016";
         boolean pprint = true;
         GoldenBirthdayYear gby = new GoldenBirthdayYear(the_year);
