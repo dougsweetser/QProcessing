e35d0ec11c4f8f7db7cec3d59e81aae210d7bf20
doug <doug@ubuntu.(none)>
2012-12-07 07:49:04 -0800

    Setting up python distribution stuff.

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "QProcessing",
+    version = "0.3.1",
+    author = "Doug Sweetser",
+    author_email = "sweetser@alum.mit.edu",
+    description = ("Visual Physics App to see the math of Nature "),
+    license = "Apache-2.0",
+    keywords = "visual physics analytic animation quaternion processing",
+    url = "http://qprocessing.atlassian.net",
+    packages=['visualphysics'],
+    long_description=read('README.txt'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
+
